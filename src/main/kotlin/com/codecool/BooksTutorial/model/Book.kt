e97bc873e4895906ae94c9df22953a36b27248e7
fanniNagy <nagy.fanni.5@gmail.com>
2020-06-11 11:23:14 +0200

    add book model

---
+++ src/main/kotlin/com/codecool/BooksTutorial/model/Book.kt
@@ -0,0 +1,16 @@
+package com.codecool.BooksTutorial.model
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+class Book(
+        @Id
+        @GeneratedValue
+        var id: Long,
+
+        var isbn: String,
+
+        var title: String
+)
