eb8fd8661232f9fc2d8b92f763b9a0d080a30e4a
fanniNagy <nagy.fanni.5@gmail.com>
2020-06-11 11:54:06 +0200

    add tests for controller

---
+++ src/test/kotlin/com/codecool/BooksTutorial/IntegrationTest.kt
@@ -0,0 +1,28 @@
+package com.codecool.BooksTutorial
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class IntegrationTest(@Autowired val restTemplate: TestRestTemplate){
+
+    @BeforeAll
+    fun setup(){
+        println(">> Startup")
+    }
+
+    @Test
+    fun `Testing Book Endpoint`(){
+        val entity = restTemplate.getForEntity<String>("/book")
+        Assertions.assertThat(entity.statusCode).isEqualTo((HttpStatus.OK))
+    }
+
+}
