2e5bfd9da36667959358023188deb73a072944a4
fanniNagy <nagy.fanni.5@gmail.com>
2020-06-11 11:37:51 +0200

    add controller

---
+++ src/main/kotlin/com/codecool/BooksTutorial/controller/BookController.kt
@@ -0,0 +1,22 @@
+package com.codecool.BooksTutorial.controller
+
+import com.codecool.BooksTutorial.model.Book
+import com.codecool.BooksTutorial.repository.BookRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class BookController(val repository: BookRepository){
+
+    @PostMapping("/book")
+    fun addBooks(@RequestBody book: Book){
+        repository.save(book)
+    }
+
+    @GetMapping("/book")
+    fun getAllBooks(): List<Book>{
+        return repository.findAll().toList();
+    }
+}
