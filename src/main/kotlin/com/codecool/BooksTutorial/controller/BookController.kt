7089990884758aa11bd6ca4218f2f1f92f12a632
fanniNagy <nagy.fanni.5@gmail.com>
2020-06-11 11:46:49 +0200

    add get book by title

--- src/main/kotlin/com/codecool/BooksTutorial/controller/BookController.kt
+++ src/main/kotlin/com/codecool/BooksTutorial/controller/BookController.kt
@@ -2,10 +2,7 @@ package com.codecool.BooksTutorial.controller
 
 import com.codecool.BooksTutorial.model.Book
 import com.codecool.BooksTutorial.repository.BookRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class BookController(val repository: BookRepository){
@@ -17,5 +14,8 @@ class BookController(val repository: BookRepository){
 
     @GetMapping("/book")
     fun getAllBooks() = repository.findAll().toList();
+
+    @GetMapping("/book/{title}")
+    fun getBookByTitle(@PathVariable("title")title: String) = repository.findByTitle(title)
 
 }
--- src/main/kotlin/com/codecool/BooksTutorial/repository/BookRepository.kt
+++ src/main/kotlin/com/codecool/BooksTutorial/repository/BookRepository.kt
@@ -3,4 +3,7 @@ package com.codecool.BooksTutorial.repository
 import com.codecool.BooksTutorial.model.Book
 import org.springframework.data.repository.CrudRepository
 
-interface BookRepository: CrudRepository<Book, Long>
+interface BookRepository: CrudRepository<Book, Long>{
+
+    fun findByTitle(title: String) : Book
+}
