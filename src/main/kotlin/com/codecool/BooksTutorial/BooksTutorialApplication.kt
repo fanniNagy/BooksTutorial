0da2fe87e1a983740833369491d428588d804c89
fanniNagy <nagy.fanni.5@gmail.com>
2020-06-11 11:18:01 +0200

    Initial commit

---
+++ src/main/kotlin/com/codecool/BooksTutorial/BooksTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.codecool.BooksTutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BooksTutorialApplication
+
+fun main(args: Array<String>) {
+	runApplication<BooksTutorialApplication>(*args)
+}
---
+++ src/test/kotlin/com/codecool/BooksTutorial/BooksTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.codecool.BooksTutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BooksTutorialApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
